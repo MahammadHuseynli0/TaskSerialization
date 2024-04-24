f98ede81d1985edd7a127ebd20726d939ace1505
MahammadHuseynli0 <tu7uv5kks.@code.edu.az>
2024-04-24 12:05:31 +0400

    upload

---
+++ TaskSerialization/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace TaskSerialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Student student = new Student();
+            student.Id = 1;
+            student.Name = "Mehemmed";
+            student.Surname = "Huseyinli";
+
+
+            string json = StudentToJSON(student);
+
+
+
+            Student deserializedStudent = JSONToStudent(json);
+
+            Console.WriteLine("JSON deserialized :");
+            Console.WriteLine($"Id: {deserializedStudent.Id}");
+            Console.WriteLine($"Name: {deserializedStudent.Name}");
+            Console.WriteLine($"SurName: {deserializedStudent.Surname}");
+        
+
+
+        }
+
+        static string StudentToJSON(Student student)
+        {
+            return JsonSerializer.Serialize(student);
+        }
+
+        static Student JSONToStudent(string json)
+        {
+            return JsonSerializer.Deserialize<Student>(json);
+        }
+    }
+
+}
+}
---
+++ TaskSerialization/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSerialization
+{
+    [Serializable]
+    internal class Student: ISerializable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Id",Id, typeof(Student));
+            info.AddValue("Name",Name, typeof(Student));
+            info.AddValue("Surname", Surname, typeof(Student));
+        }
+    }
+}
